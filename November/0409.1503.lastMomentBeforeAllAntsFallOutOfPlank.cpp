9d726be5ba24078136aa6a19cb9cb10be03021c7
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-11-06 22:08:29 +0530

    Create 0409.1503.lastMomentBeforeAllAntsFallOutOfPlank.cpp

---
+++ November/0409.1503.lastMomentBeforeAllAntsFallOutOfPlank.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int result =0 ; 
+        for(int i=0 ; i <left.size(); i++){
+            result= max(result , left[i]);
+        }
+        for(int i = 0; i <right.size(); i++){
+            result = max(result , n-right[i]);
+        }
+        return result;
+    }
+};
