820fea3f046b5e6a2ab98b6261f6015d785bc6f3
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-11-09 23:03:16 +0530

    Create 09Nov.1759.CountNumberOfHomogeneousSubStrings.cpp

---
+++ November/09Nov.1759.CountNumberOfHomogeneousSubStrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mod = 1e9+7;
+    int countHomogenous(string s) {
+        long long ans =0 ;
+        cout<<s.length();
+        for(int i =0 ; i <s.length();i++){
+            string str ="";
+            int j =i ; 
+            while(i<s.length() && s[j]==s[i]){
+                str+=s[i];
+                i++;
+            }
+            long long len = str.length();
+            while(len>0){
+                ans = (ans+ len)%mod;
+                len--;
+            }
+            i--;
+        }
+        return ans;
+    }
+};
